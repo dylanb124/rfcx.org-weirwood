5d579fd56b35ad933a55a00aa13288facc3bab08
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-11-30 13:09:30 +0300

    create env file for staging; add npm commands for running and building staging app

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -1,7 +1,8 @@
 import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
-  ENV: 'PROD'
+  ENV: 'PROD',
+  API: 'https://api.rfcx.org/v1/'
 };
 
 export = ProdConfig;
---
+++ tools/env/staging.ts
@@ -0,0 +1,9 @@
+import { EnvConfig } from './env-config.interface';
+
+const StagingConfig: EnvConfig = {
+  ENV: 'STAGING',
+  API: 'https://staging-api.rfcx.org/v1/'
+};
+
+export = StagingConfig;
+
