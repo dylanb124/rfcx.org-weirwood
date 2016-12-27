23653ddfaf60559a99e788143e1802e5c8445bfd
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-12-27 12:40:39 +0300

    scroll page to top on route change (using new dimensions config)

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { UserService } from '../user/user.service';
+import { APP_CONFIG, IAppConfig } from '../../app.config';
+
+let jQuery: any = (window as any)['$'];
 
 @Component({
   moduleId: module.id,
@@ -12,8 +15,18 @@ export class NavbarComponent {
 
     constructor(
         private userService: UserService,
-        private router: Router
-    ) {}
+        private router: Router,
+        @Inject(APP_CONFIG) private config: IAppConfig
+    ) {
+        console.log('app configggg', this.config);
+        router.events.subscribe((val) => {
+            if (val instanceof NavigationEnd) {
+                if (jQuery(window).width() <= (this.config.dimensions.phone.max || 767)) {
+                    window.scrollTo(0,0);
+                }
+            }
+        });
+    }
 
     logOut() {
         this.userService.logOut();
