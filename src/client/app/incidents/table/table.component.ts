0cdec0a4a18a13321ac56bc00fdd070fe908196b
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-01-13 16:12:03 +0300

    bind table component to API data

--- src/client/app/incidents/table/table.component.ts
+++ src/client/app/incidents/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 // let jQuery: any = (window as any)['$'];
 
@@ -11,54 +11,11 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class IncidentsTableComponent implements OnInit {
 
-    // @Input() private data: Array<any>;
-    private data: Array<any>;
+    @Input() private data: Array<any>;
     private labels: Array<string>;
     private labelsTotal: any;
 
     ngOnInit() {
-        this.data = [
-            {
-                title: 'Forest Sector 1',
-                incidents: {
-                    chainsaws: 10,
-                    shots: 10,
-                    vehicles: 10
-                }
-            },
-            {
-                title: 'Forest Sector 2',
-                incidents: {
-                    chainsaws: 20,
-                    shots: 20,
-                    vehicles: 20
-                }
-            },
-            {
-                title: 'Forest Sector 3',
-                incidents: {
-                    chainsaws: 30,
-                    shots: 30,
-                    vehicles: 30
-                }
-            },
-            {
-                title: 'Forest Sector 4',
-                incidents: {
-                    chainsaws: 40,
-                    shots: 40,
-                    vehicles: 40
-                }
-            },
-            {
-                title: 'Forest Sector 5',
-                incidents: {
-                    chainsaws: 50,
-                    shots: 50,
-                    vehicles: 50
-                }
-            }
-        ];
         this.getAllLabels();
         this.calculateTotalPerSector();
         this.calculateTotalPerLabel();
@@ -67,7 +24,7 @@ export class IncidentsTableComponent implements OnInit {
     getAllLabels() {
         this.labels = [];
         this.data.forEach((item) => {
-            Object.keys(item.incidents).forEach((label) => {
+            Object.keys(item.events).forEach((label) => {
                 if (this.labels.indexOf(label) === -1) {
                     this.labels.push(label);
                 }
@@ -77,7 +34,7 @@ export class IncidentsTableComponent implements OnInit {
 
     calculateTotalPerSector() {
         this.data.forEach((item) => {
-            item.total = Object.values(item.incidents).reduce((a:string, b:string) => {
+            item.total = Object.values(item.events).reduce((a:string, b:string) => {
                 return parseInt(a) + parseInt(b);
             }, 0);
         });
@@ -88,7 +45,9 @@ export class IncidentsTableComponent implements OnInit {
         this.labels.forEach((label) => {
             this.labelsTotal[label] = 0;
             this.data.forEach((item) => {
-                this.labelsTotal[label] += item.incidents[label];
+                if (item.events[label]) {
+                    this.labelsTotal[label] += item.events[label];
+                }
             });
         });
         this.labelsTotal.total = 0;
